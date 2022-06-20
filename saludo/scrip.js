746cbb9570d609b24e0eab03adddbd7be467f44f
Cristofer Pineda <cristofer.pineda.jauregui@gmail.com>
2022-06-19 19:57:30 -0500

    Se realiza el cambio de h2

--- saludo/scrip.js
+++ saludo/scrip.js
@@ -1,2 +1,21 @@
 console.log("Se nota que todabia me extrañas");
 
+
+setInterval( cambiarSaludo, 2000);
+
+function cambiarSaludo(){
+    //console.log("Me parece una falta de respeto");
+    let frase = document.getElementsByTagName("h2")[1].innerHTML
+
+
+if( document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre , mis cielas"){
+    //frase = "Impongo moda para las niñas"
+    document.getElementsByTagName("h2")[1].innerHTML= "Impongo moda para las niñas!";
+}
+else{
+    //frase = "Ganando como siempre , mis cielas";
+    document.getElementsByTagName("h2")[1].innerHTML = "Ganando como siempre , mis cielas";
+
+}
+console.log("frase :" + frase);
+}
