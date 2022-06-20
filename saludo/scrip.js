084023be8a7ccd4309eab5a5d673664232ec91bc
Cristofer Pineda <cristofer.pineda.jauregui@gmail.com>
2022-06-19 20:09:52 -0500

    Se utiliza innert para cambio de h2

--- saludo/scrip.js
+++ saludo/scrip.js
@@ -3,6 +3,9 @@ console.log("Se nota que todabia me extrañas");
 
 setInterval(cambiarSaludo, 2000);
 
+/**
+ * Cambia la frase en el titulo h2
+ */
 function cambiarSaludo() {
     let frase = document.getElementsByTagName("h2")[1];
     console.log(frase);
